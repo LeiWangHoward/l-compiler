8ec76b66e1e0046e7b23dfd9524e743f72f48af4
Lei Wang <lwanghoward@gmail.com>
2013-05-29 20:59:27 -0500

    now 92% on robby, coooooolll

--- L5/L5-parse-compile.rkt
+++ L5/L5-parse-compile.rkt
@@ -49,6 +49,16 @@
              (back-one)
              (L5-compile (L5_lambda `(,x) (L5-parse `(,prim_op ,x)))))])))
 
+#|(define (new-lambda e)
+  (let ([prim_op (L5-compile e)]
+        [x (fresh-var 'lam_x)]
+        [y (fresh-var 'lam_y)])
+    (if (biop? prim_op) 
+        (L5-compile (L5_lambda `(,x ,y) (L5-parse `(,prim_op ,x ,y))))
+        (begin (back-one)
+               (back-one)
+               (L5-compile e)))))|#
+
 (module+ test
   (test (new-procedure ':f1 '(x) '(y) '(- (+ x y) x)) '(:f1 (vars-tup x) 
                                                             (let ((y (aref vars-tup 0))) 
@@ -67,11 +77,17 @@
     [`(lambda ,args ,e)
      (L5_lambda args (L5-parse e))]
     [`(let ((,var ,e1)) ,e2)
-     (L5_let var (L5-parse e1) (L5-parse e2))]
+     (let* ([level_var (level-var var)]
+            [new_e2 (replace e2 var level_var)])
+       (L5_let level_var (L5-parse e1) (L5-parse new_e2)))]
     [`(letrec ((,x ,e1)) ,e2)
-     (L5_let x (L5-parse `(new-tuple 0)) 
-             (L5_begin (L5-parse `(aset ,x 0 ,(replace e1 x `(aref ,x 0))))
-                       (L5-parse (replace e2 x `(aref ,x 0)))))] 
+     (let* ([level_x (level-var x)]
+            [new_e1 (replace e1 x level_x)]
+            [new_e2 (replace e2 x level_x)])
+       (L5_letrec level_x (L5-parse new_e1) (L5-parse new_e2)))]
+    ;(L5_let x (L5-parse `(new-tuple 0)) 
+    ;        (L5_begin (L5-parse `(aset ,x 0 ,(replace e1 x `(aref ,x 0))))
+    ;                  (L5-parse (replace e2 x `(aref ,x 0)))))] 
     ;(L5_letrec var (L5-parse e1) (L5-parse e2))]
     [`(if ,cond ,then ,else)
      (L5_if (L5-parse cond)
@@ -136,7 +152,10 @@
                               ,(replace (L5-compile e2) x new_lab))))|#
             `(let ([,x ,(L5-compile e1)])
                ,(L5-compile e2)))
-    ;(L5_letrec (x e1 e2) ;eliminated in parse step
+    (L5_letrec (x e1 e2) 
+               `(let ((,x (new-tuple 0)))
+                  (begin (aset ,x 0 ,(replace (L5-compile e1) x `(aref ,x 0)))
+                         ,(replace (L5-compile e2) x `(aref ,x 0)))))
     (L5_if (e1 e2 e3)
            `(if ,(L5-compile e1)
                 ,(L5-compile e2)
@@ -167,10 +186,12 @@
                                [label (label-it app_name)])
                           `(let ([,app_name ,(L5-compile (first e_lst))])
                              ((closure-proc ,app_name) (closure-vars ,app_name)
-                                                       (new-tuple ,(map (λ (ele)
-                                                                          (L5-compile ele))
-                                                                        args))))))))))
-    (else L5-parsed)))
+                                                       ,(cons 'new-tuple (map (λ (ele)
+                                                                                (if (L5_prim? ele)
+                                                                                    (new-lambda ele)
+                                                                                    (L5-compile ele)))
+                                                                              args))))))))))
+    ))
 ;(else L5-parsed)))
 (module+ test 
   (test (L5-compile (L5-parse 'f)) 'f)
@@ -182,11 +203,11 @@
                 (L5_app (list (L5_x 'f) (L5_num 1)))))
   (test (L5-compile (L5-parse `(print 1))) '(print 1))
   (test (L5-compile (L5-parse `(f 1 2))) '(let ((new_app0 f)) ((closure-proc new_app0) (closure-vars new_app0) 1 2)))
-  (test (L5-compile (L5-parse `(let ((a 1)) a))) '(let ((a 1)) a))
+  (test (L5-compile (L5-parse `(let ((a 1)) a))) '(let ((a_1 1)) a_1))
   (test (L5-compile (L5-parse `(letrec ([x (+ x 1)]) (+ x 2)))) 
-        '(let ((x (new-tuple 0))) (begin (aset x 0 (+ (aref x 0) 1)) (+ (aref x 0) 2))))
+        '(let ((x_1 (new-tuple 0))) (begin (aset x_1 0 (+ (aref x_1 0) 1)) (+ (aref x_1 0) 2))))
   (test (L5-compile (L5-parse `(if (< x 1) (+ x 2) (+ x 3)))) '(if (< x 1) (+ x 2) (+ x 3)))
   (test (L5-compile (L5-parse `(let ([f (lambda (y) (+ x y))])
-                                 (f 1))))  '(let ((f (make-closure :new_app1 (new-tuple x))))
-                                              (let ((new_app2 f)) 
+                                 (f 1))))  '(let ((f_1 (make-closure :new_app1 (new-tuple x))))
+                                              (let ((new_app2 f_1)) 
                                                 ((closure-proc new_app2) (closure-vars new_app2) 1))))) 
--- L5/temp-util.rkt
+++ L5/temp-util.rkt
@@ -20,6 +20,10 @@
   (string->symbol
    (format "~a~a~a" var-pre var (count-one))))
 
+(define (level-var var)
+  (string->symbol
+   (format "~a~a~a" var '_ 1)))
+
 (define (label-it var)
   (string->symbol
    (format ":~a" (symbol->string var))))
@@ -27,4 +31,5 @@
 (module+ test
   (test (fresh-var 'x) 'var_x0)
   (test (label-it 'a) ':a)
+  (test (level-var 'me) 'me_1)
   (test (fresh-app) 'new_app1))
